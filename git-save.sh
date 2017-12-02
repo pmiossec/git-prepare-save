4317a28254e2ad517b49ba26a742ae308f86dafd
Philippe Miossec <pmiossec@gmail.com>
2017-12-02 13:37:08 +0100

    working version

--- git-save.sh
+++ git-save.sh
@@ -1,8 +1,15 @@
 if [ ! -d ".git" ]; then
-  echo "not a git repository"
+  echo "Not a git repository!!!"
   exit 0
 fi
 
-echo "processing git repository"
-exit 0
-git stash save "Saving in stash"
+echo "=> Processing git repository..."
+
+echo "=> Stashing not commited changes..."
+git stash save "Saving in stash" --include-untracked
+git gc --aggressive --force
+echo "=> Will delete:"
+find . -maxdepth 1 \! \( -name .git -o -name . -o -name .. \)
+find . -maxdepth 1 \! \( -name .git -o -name . -o -name .. \)  -exec rm -Rf '{}' \;
+
+
